e12d39d96b5f0a8dc802d457fd02a3566eaf6363
Your Name <you@example.com>
2024-05-17 21:41:49 +0800

    compress 测试

--- BGP_3/cmake-build-debug/.qt/QtDeploySupport.cmake
+++ BGP_3/cmake-build-debug/.qt/QtDeploySupport.cmake
@@ -34,7 +34,7 @@ endif()
 set(__QT_DEPLOY_SYSTEM_NAME "Windows")
 set(__QT_DEPLOY_IS_SHARED_LIBS_BUILD "ON")
 set(__QT_DEPLOY_TOOL "C:/Qt/6.8.0/msvc2019_64/bin/windeployqt.exe")
-set(__QT_DEPLOY_IMPL_DIR "D:/M/MATLAB Driver/QT_CLION/T1/cmake-build-debug/.qt")
+set(__QT_DEPLOY_IMPL_DIR "D:/M/MATLAB Driver/QT_CLION/BGP3/BGP_3/cmake-build-debug/.qt")
 set(__QT_DEPLOY_VERBOSE "")
 set(__QT_CMAKE_EXPORT_NAMESPACE "Qt6")
 set(__QT_DEPLOY_GENERATOR_IS_MULTI_CONFIG "0")
@@ -58,7 +58,7 @@ set(__QT_DEPLOY_QT_DEBUG_POSTFIX "d")
 
 # Define the CMake commands to be made available during deployment.
 set(__qt_deploy_support_files
-    "D:/M/MATLAB Driver/QT_CLION/T1/cmake-build-debug/.qt/QtDeployTargets.cmake"
+    "D:/M/MATLAB Driver/QT_CLION/BGP3/BGP_3/cmake-build-debug/.qt/QtDeployTargets.cmake"
     "C:/Qt/6.8.0/msvc2019_64/lib/cmake/Qt6Core/Qt6CoreDeploySupport.cmake"
 )
 foreach(__qt_deploy_support_file IN LISTS __qt_deploy_support_files)
--- BGP_3/compress.cpp
+++ BGP_3/compress.cpp
@@ -1,3 +1,20 @@
 //
 // Created by HP183 on 2024/5/16.
 //
+#include "istream"
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    int cnt = 1;
+    for (int i = 1; i < n; i++) {
+        if (a[i] != a[i - 1]) {
+            cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
