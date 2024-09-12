4cee56bd888efe5e6b1d47e8ea4843a3dfd5f3be
Your Name <you@example.com>
2024-09-12 20:09:25 +0800

    Deleted BGP_3 folder

--- BGP_3/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_T1_FILE D:/M/MATLAB Driver/QT_CLION/BGP3/BGP_3/cmake-build-debug/T1.exe)
-set(__QT_DEPLOY_TARGET_T1_RUNTIME_DLLS C:/Qt/6.8.0/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.0/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.8.0/msvc2019_64/bin/Qt6Cored.dll)
